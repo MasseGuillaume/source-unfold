950743fd7fec1f6670777eaa5afa0fec96a29e00
Guillaume Massé <masgui@gmail.com>
2018-08-03 02:23:38 +0200

    .

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,101 @@ import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl._
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration.Duration
+
+import scala.collection.mutable
+import scala.util.{Success, Failure, Try}
+
+import akka.stream.SourceShape
+import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
+
+object Extensions {
+  implicit class QueueExtensions[A](private val self: mutable.Queue[A]) extends AnyVal {
+    def dequeueN(n: Int): List[A] = {
+      val b = List.newBuilder[A]
+      var i = 0
+      while (i < n) {
+        val e = self.dequeue
+        b += e
+        i += 1
+      }
+      b.result()
+    }
+  }
+}
+
+class UnfoldSource[S, E](seeds: List[S],
+                         flow: Flow[S, E, NotUsed])(loop: E => List[S])(bufferSize: Int)(implicit ec: ExecutionContext) extends GraphStage[SourceShape[E]] {
+
+  import Extensions.QueueExtensions
+  val out: Outlet[E] = Outlet("UnfoldSource.out")
+  override val shape: SourceShape[E] = SourceShape(out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) with OutHandler {  
+    val frontier = mutable.Queue[S]()
+    val buffer = mutable.Queue[E]()
+    var inFlight = 0
+
+    def isBufferFull() = buffer.size >= bufferSize
+    frontier ++= seeds
+    var downstreamWaiting = false
+
+    def fillBuffer(): Unit = {
+      val batchSize = Math.min(bufferSize - buffer.size, frontier.size)
+      val batch = frontier.dequeueN(batchSize)
+      inFlight += batchSize
+
+      val toProcess =
+        Source(batch)
+          .via(flow)
+          .runWith(Sink.seq)(materializer)
+
+      val callback = getAsyncCallback[Try[Seq[E]]]{
+        case Failure(ex) => {
+          fail(out, ex)
+        }
+        case Success(es) => {
+          inFlight -= es.size
+          es.foreach{ e =>
+            buffer += e
+            frontier ++= loop(e)
+          }
+          if (downstreamWaiting && buffer.nonEmpty) {
+            val e = buffer.dequeue
+            downstreamWaiting = false
+            sendOne(e)
+          }
+          ()
+        }
+      }
+
+      toProcess.onComplete(callback.invoke)
+    }
+    override def preStart(): Unit = fillBuffer()
+
+    def sendOne(e: E): Unit = {
+      push(out, e)
+      if (inFlight == 0 && buffer.size == 0 && frontier.size == 0) {
+        completeStage()
+      }
+    }
+
+    def onPull(): Unit = {
+      if (buffer.nonEmpty) {
+        sendOne(buffer.dequeue)
+      } else {
+        downstreamWaiting = true
+      }
+
+      if (!isBufferFull) {
+        fillBuffer()
+      }
+    }
+
+    setHandler(out, this)
+  }
+}
 
 object Main {
   type ItemId = Int
@@ -28,23 +121,22 @@ object Main {
 
 
   implicit val system = ActorSystem()
+  import system.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  def unfold[S, E](seed: S, flow: Flow[S, E, NotUsed])(loop: E => List[S]): Source[E, NotUsed] = {
-    // todo: recurse
-    Source.single(seed).via(flow)
+  def unfold[S, E](seeds: List[S], flow: Flow[S, E, NotUsed])(loop: E => List[S]): Source[E, NotUsed] = {
+    Source.fromGraph(new UnfoldSource(seeds, flow)(loop)(bufferSize = 100))
   }
 
   def main(args: Array[String]): Unit = {
 
     val items = List.newBuilder[Item]
     val keep = Sink.foreach{(x: Item) =>
-      println(x)
       items += x
     }
 
     Await.result(
-      unfold(0, itemFlow)(_.kids).runWith(keep),
+      unfold(List(0), itemFlow)(_.kids).runWith(keep),
       Duration.Inf
     )
 
